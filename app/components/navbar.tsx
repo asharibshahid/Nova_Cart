3a77483d91e820368fa35e6d1b2460680f459aa9
ASHARIB-SHEIKH👩🏻‍💻 <160227183+asharibshahid@users.noreply.github.com>
2025-03-21 08:59:09 +0500

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, {  useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useUser, SignInButton, SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
